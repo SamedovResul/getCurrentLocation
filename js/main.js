ea9b609c7d199ecce93f1da43e041ece84aa943e
SamedovRasul <samedovrasul7@gmail.com>
2022-04-19 16:47:42 +0400

    test

--- js/main.js
+++ js/main.js
@@ -1,15 +1,55 @@
-function getLocation() {
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-      alert("Geolocation is not supported by this browser.");
+let coordinat = {
+  lat:'',
+  lng:''
+}
+
+setInterval(() => {
+  function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
   }
-}
+  
+  function showPosition(position) {
+    coordinat ={
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    }
+  // a = position.coords.latitude + ',' + position.coords.longitude;
+  
+  // alert(a);
+  }
+  const socket = io('ws://localhost:5000', { transports : ['websocket'] });
+  socket.emit("device", coordinat)
+  console.log(coordinat)
+}, 2000);
 
-function showPosition(position) {
-var a = position.coords.latitude + ',' + position.coords.longitude;
+// function getLocation() {
+//   if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//       alert("Geolocation is not supported by this browser.");
+//   }
+// }
 
-alert(a);
-}
+// function showPosition(position) {
+//   coordinat ={
+//     lat: position.coords.latitude,
+//     lng: position.coords.longitude
+//   }
+// // a = position.coords.latitude + ',' + position.coords.longitude;
 
-getLocation()
+// // alert(a);
+// }
+// getLocation()
+
+// const socket = new WebSocket('ws://localhost:5000');
+
+// let coords = {
+//   lat:40.376435,
+//   lng:49.815670
+// }
+
+
